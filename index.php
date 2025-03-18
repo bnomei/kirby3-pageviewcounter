806f1eeed1d1e83ba90e22080ef1cdc03f5c3fbc
bnomei <bnomei@users.noreply.github.com>
2025-03-18 19:50:33 +0000

    Fix styling

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ Kirby::plugin('bnomei/pageviewcounter', [
         'pageviewcount' => function (): int {
             return \Bnomei\PageViewCounter::singleton()->count($this->id()); // id NOT uuid!
         },
-        'lastvisited' => function ($format = 'c'): string { //for format options see https://www.php.net/manual/en/function.date.php
+        'lastvisited' => function ($format = 'c'): string { // for format options see https://www.php.net/manual/en/function.date.php
             return date($format, \Bnomei\PageViewCounter::singleton()->timestamp($this->id())); // id NOT uuid!
         },
         'counterImage' => function () {
